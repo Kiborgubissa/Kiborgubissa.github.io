cebcea9e29d83e1ba407f1a7c285330cc4b5e7e7
Kiborgubissa <106691902+Kiborgubissa@users.noreply.github.com>
2023-04-04 20:20:27 +0300

    change Aperture

---
+++ Aperture/js/script.js
@@ -0,0 +1,14 @@
+function ibg(){
+    if(document.querySelectorAll(".ibg img")){
+        for(el of document.querySelectorAll(".ibg")){
+            el.setAttribute("style",`background-image: url('${el.querySelector('img').getAttribute('src')}')`)
+        }
+    }
+}
+ibg()
+const burg = document.querySelector('.header__burger')
+burg.addEventListener('click',()=>{
+  burg.classList.toggle('active')
+  document.querySelector('.header__body').classList.toggle('active')
+  document.body.classList.toggle('body-lock')
+})
